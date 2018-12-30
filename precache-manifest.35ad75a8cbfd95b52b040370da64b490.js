f8468152ca14c8f403a01e78090cc1bb593154b5
kamilmoskal <kamil_moskal@interia.pl>
2018-12-30 11:30:36 +0100

    Updates

---
+++ precache-manifest.35ad75a8cbfd95b52b040370da64b490.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "38729b6abf2c9eb608fc4f4b1a075be3",
+    "url": "/react-hangman/static/media/3.38729b6a.png"
+  },
+  {
+    "revision": "1c7ea7701b040e93d9fb",
+    "url": "/react-hangman/static/css/main.15995b0b.chunk.css"
+  },
+  {
+    "revision": "c18b5c890581e68ff9a8",
+    "url": "/react-hangman/static/js/1.c18b5c89.chunk.js"
+  },
+  {
+    "revision": "c0d69552ac1fc5b0099d",
+    "url": "/react-hangman/static/js/runtime~main.c0d69552.js"
+  },
+  {
+    "revision": "573eee9418f58449031d8314b7e50bb5",
+    "url": "/react-hangman/static/media/0.573eee94.png"
+  },
+  {
+    "revision": "43a072c7996e5df2bbb10712441dc3c1",
+    "url": "/react-hangman/static/media/bg.43a072c7.jpg"
+  },
+  {
+    "revision": "8a13182cf86bea3f59a20c1d9cfec8ff",
+    "url": "/react-hangman/static/media/1.8a13182c.png"
+  },
+  {
+    "revision": "447fef9d15832339d8e2460299b2a0b9",
+    "url": "/react-hangman/static/media/2.447fef9d.png"
+  },
+  {
+    "revision": "1c7ea7701b040e93d9fb",
+    "url": "/react-hangman/static/js/main.1c7ea770.chunk.js"
+  },
+  {
+    "revision": "9995da8a15d9348ad3353b7146116c46",
+    "url": "/react-hangman/static/media/4.9995da8a.png"
+  },
+  {
+    "revision": "8d06e28da557fb3d0b4e6ee5b7dc9897",
+    "url": "/react-hangman/static/media/5.8d06e28d.png"
+  },
+  {
+    "revision": "24dc1805996aa165acb5c6c3b2b7897a",
+    "url": "/react-hangman/static/media/6.24dc1805.png"
+  },
+  {
+    "revision": "41b0424009807c7da37a6eabfdb79a73",
+    "url": "/react-hangman/static/media/7.41b04240.png"
+  },
+  {
+    "revision": "95bceca94bb1f79528c31d68e22a6c7a",
+    "url": "/react-hangman/static/media/8.95bceca9.png"
+  },
+  {
+    "revision": "5a3de260ab525b7a489ddf4d40d37b23",
+    "url": "/react-hangman/static/media/9.5a3de260.png"
+  },
+  {
+    "revision": "e93d011da831b2548e95814c63ac924b",
+    "url": "/react-hangman/index.html"
+  }
+];
